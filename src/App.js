70e0bb7874e9ec9d2c3fb1249ddb7fb7cd8c23a4
tewarishash12 <tewarishashwat2002@gmail.com>
2024-12-05 16:31:50 +0530

    Nabar is created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="font-bold underline">
-      Explorin
+    <div className="">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import icon from "../assets/Pic in College Dress.jpg";
+import { FaBriefcase, FaBell } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-around items-center border-b-2 border-grey-500 h-[70px]'>
+            <div className='flex items-center gap-4'>
+                <FaBriefcase size={24} color="orange" />
+                <div className="heading text-xl font-bold">Explorin Solutions</div>
+            </div>
+
+            <div className='flex items-center gap-8'>
+                <div className='flex items-center gap-2'>
+                    <input type="checkbox" id="saved" className='h-[20px] w-[20px]' />
+                    <label htmlFor="saved" className='text-sm'>My saved jobs only</label>
+                </div>
+
+                <FaBell size={24} color="black" />
+
+                <img src={icon} alt="icon" className='h-[40px] w-[40px] rounded-full' />
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
