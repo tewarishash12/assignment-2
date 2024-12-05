a7d81e3e6ec54cece2bcee521f1aacd593814bca
tewarishash12 <tewarishashwat2002@gmail.com>
2024-12-05 17:40:18 +0530

    Added hover effect on card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const JobCard = ({ jobRole, workType, salary, companyIcon, companyName, location }) => {
     return (
-        <div className="bg-white shadow-lg p-4 rounded-lg border border-gray-200">
+        <div className="bg-white shadow-lg p-4 rounded-lg border border-gray-200 transition-all duration-300 hover:bg-gradient-to-r hover:from-orange-200 hover:to-white">
             <div className="text-lg font-semibold">{jobRole}</div>
             <div className="text-sm text-green-600 font-medium my-1">{workType}</div>
             <div className="text-sm text-gray-600">Salary: {salary}</div>
